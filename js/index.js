5c837ccc031f71ab7a4128fc6dd9ec8e5195c9a3
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-17 23:19:15 +0530

    added some methods

--- js/index.js
+++ js/index.js
@@ -1,16 +1,57 @@
-const keyboardRows = document.querySelector(".row");
 import { answerObject, answerArray, eligibleObject } from "/js/array.js";
-console.log(answerObject);
+
 class Board {
     constructor() {
-        this._boardArray = 1;
+        this.boardArray = [
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+        ];
+        this.row = 0;
+        this.col = 0;
     }
 
-    get array() {
-        return this._boardArray;
+    goAhead() {
+        if (this.col < 4) {
+            this.col += 1;
+        }
+    }
+
+    goBack() {
+        if (this.col > 0) {
+            this.col -= 1;
+        }
+    }
+
+    addLetter(key) {
+        this.boardArray[this.row][this.col] = key;
+        document.getElementById(`${this.row}${this.col}`).textContent = key;
+        this.goAhead();
+    }
+
+    removeLetter() {
+        this.boardArray[this.row][this.col] = "";
+        document.getElementById(`${this.row}${this.col}`).textContent = null;
+        this.goBack();
     }
 }
 
-let board = new Board();
+const board = new Board();
 
-// keyboardRows.addEventListener("click", board.addKeyPress);
+const addKeyPress = (event) => {
+    console.log(event.target);
+    if (event.target.id !== "ENTER" && event.target.id !== "DEL") {
+        board.addLetter(event.target.id);
+    } else if (event.target.id === "DEL") {
+        board.removeLetter();
+    }
+};
+
+const keyboardButtonArray = document.getElementsByClassName("keyboard-button");
+
+for (let keyboardButton of keyboardButtonArray) {
+    keyboardButton.addEventListener("click", addKeyPress);
+}
