5ee889dd2b5a0c2cfbf2c9cb0326beaa2d2a8fd1
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-17 22:23:48 +0530

    added the necessary arrays and objects

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world", 12);
+const keyboardRows = document.querySelector(".row");
+
+class Board {
+    constructor() {
+        this._boardArray = 1;
+    }
+
+    get array() {
+        return this._boardArray;
+    }
+}
+
+let board = new Board();
+
+console.log(board.array);
+// keyboardRows.addEventListener("click", board.addKeyPress);
