f6875695622c1f65c91c4ba9a99fc1683e26e9a3
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-18 19:14:22 +0530

    added processWord methos

--- js/index.js
+++ js/index.js
@@ -2,9 +2,23 @@ import { answerArray, eligibleObject } from "/js/array.js";
 
 class Board {
     constructor() {
-        this.boardArray = ["", "", "", "", ""];
+        this.boardArray = [];
         this.currentWord = "";
         this.wordCounter = 0;
+
+        let randomWord = answerArray[272].toUpperCase();
+        console.log(randomWord, "randomWord");
+
+        this.matchWord = () => {
+            console.log(randomWord, this.currentWord);
+            return randomWord
+                .split("")
+                .map((actualLetter, index) =>
+                    this.currentWord.charAt(index) === actualLetter
+                        ? true
+                        : false
+                );
+        };
     }
 
     addLetter(letter) {
@@ -16,7 +30,7 @@ class Board {
         }
     }
 
-    removeLetter(letter) {
+    removeLetter() {
         if (this.currentWord.length > 0) {
             document.getElementById(
                 `${this.wordCounter}${this.currentWord.length - 1}`
@@ -27,6 +41,17 @@ class Board {
             );
         }
     }
+
+    processWord() {
+        if (this.currentWord.length === 5 && this.wordCounter < 6) {
+            console.log(this.matchWord);
+            changeBoardRow();
+            this.boardArray.push(this.matchWord());
+            this.wordCounter += 1;
+        }
+    }
+
+    changeBoardRow() {}
 }
 
 const board = new Board();
@@ -38,7 +63,7 @@ const keyboardButtonHandler = (event) => {
     } else if (event.target.id === "DEL") {
         board.removeLetter();
     } else if (event.target.id === "ENTER") {
-        board.matchWord();
+        board.processWord();
     }
 };
 
